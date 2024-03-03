af5e7507c003a649fb825ac732ee0404fd11d0ff
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2024-03-03 14:15:08 +0000

    add create and free matrix

---
+++ matrix.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrix.h"
+
+Matrix *create_matrix(int rows, int cols) {
+    Matrix *m = malloc(sizeof(Matrix));
+    m->rows = rows;
+    m->cols = cols;
+    m->data = malloc(rows * sizeof(double *));
+    for (int i = 0; i < rows; i++) {
+        m->data[i] = calloc(cols, sizeof(double));
+    }
+    return m;
+}
+
+void free_matrix(Matrix *m) {
+    if (!m) return;
+    for (int i = 0; i < m->rows; i++)
+        free(m->data[i]);
+    free(m->data);
+    free(m);
+}
